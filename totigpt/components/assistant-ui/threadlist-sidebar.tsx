27de93c67ee2786af99c0ba58079efb0f840ef31
totigpt <ismailriaz667@gmail.com>
2026-01-03 22:56:36 +0500

    Update threadlist-sidebar.tsx

--- totigpt/components/assistant-ui/threadlist-sidebar.tsx
+++ totigpt/components/assistant-ui/threadlist-sidebar.tsx
@@ -52,7 +52,7 @@ export function ThreadListSidebar({
       <SidebarMenuButton size="lg" asChild>
         <div className="flex flex-col items-center justify-center p-2">
           <img
-            src="/NovaAI.png"        // <-- Put your logo image in /public/logo.png
+            src="/Nova.png"        // <-- Put your logo image in /public/logo.png
             alt="NovaAI"
             className="w-10 h-10 mb-1 rounded-full"
           />
